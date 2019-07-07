e9eeb0698fad7146705edb3480d139c719e86e27
Leontin Vlad <33200411+leontinv@users.noreply.github.com>
2019-07-07 13:29:49 +0300

    Add files via upload

---
+++ lifescorescript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+public class lifescorescript : MonoBehaviour
+{
+    public static int scoreLifeValue = 3;
+   
+    private TMP_Text scoreLife;
+
+    void Start()
+    {
+       
+        scoreLife = GetComponent<TMP_Text>();
+        scoreLifeValue = 3;
+    }
+
+    
+    void Update()
+    {
+        scoreLife.text = "" + scoreLifeValue;
+        EndGame();
+    }
+
+    void EndGame()
+    {
+        if(scoreLifeValue <= 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
+
---
+++ minusonelife.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class minusonelife : MonoBehaviour
+{
+    public GameObject particles;
+    public GameObject plusfive;
+
+
+    void OnTriggerEnter2D(Collider2D player)
+    {
+        if (player.CompareTag("player"))
+        {
+            Destroy(gameObject);
+            Instantiate(particles, transform.position, Quaternion.identity);
+            Instantiate(plusfive, transform.position, Quaternion.identity);
+
+            
+            
+
+
+        }
+
+        if(player.CompareTag("edge"))
+        {
+            lifescorescript.scoreLifeValue -= 1;
+        }
+    }
+
+}
