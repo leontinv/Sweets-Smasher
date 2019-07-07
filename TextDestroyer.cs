4b473c64b1c5bf2e70d7c900ba62d37858f79caa
Leontin Vlad <33200411+leontinv@users.noreply.github.com>
2019-07-07 13:25:10 +0300

    Add files via upload

---
+++ TextDestroyer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextDestroyer : MonoBehaviour
+{
+    private float destroytime = 1f;
+
+    void Start()
+    {
+        Destroy(gameObject, destroytime);
+    }
+
+    
+}
