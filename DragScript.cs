823db792ea9926dad08be6e3d949ecbdc665c691
Leontin Vlad <33200411+leontinv@users.noreply.github.com>
2019-07-07 13:26:45 +0300

    Add files via upload

---
+++ DragScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragScript : MonoBehaviour
+{
+    private bool press;
+    public Rigidbody2D ballRigidBody;
+    Vector2 startPos, endPos, direction;
+    void Update()
+    {
+        if (press == true)
+        {
+            Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector2(cursorPos.x, cursorPos.y);
+            Debug.Log(transform.position);
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            endPos = transform.position;
+            direction = endPos - startPos;
+            ballRigidBody.AddForce(direction/2f * 5f);
+            press = false;
+            ballRigidBody.isKinematic = false;
+        }
+    }
+
+    void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            startPos = transform.position;
+            press = true;
+        }
+    }
+}
---
+++ PlayerCollider.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerCollider : MonoBehaviour
+{
+   
+    public GameObject particles;
+    public GameObject plusfive;
+   
+    
+
+    void Start()
+    {
+       
+    }
+   
+   void OnTriggerEnter2D (Collider2D player)
+    {
+        if (player.CompareTag("player"))
+        {
+            Destroy(gameObject);
+            Instantiate(particles, transform.position, Quaternion.identity);
+            Instantiate(plusfive, transform.position, Quaternion.identity);
+           
+            ScoreScript.scoreValue += 5;
+            SliderAdd.sliderValue.value += 5f;
+            
+           
+        }
+     /*   else
+        if (player.CompareTag("edge"))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }*/
+
+    }
+
+    
+  
+}
